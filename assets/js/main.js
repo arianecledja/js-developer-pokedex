bd33dc002610fdbc3f06b27dae5da94290a049bc
Ariane Santos <ariane225040@gmail.com>
2023-10-22 04:41:44 -0300

    add details.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,45 +3,87 @@ const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 151
 const limit = 10
-let offset = 0;
+let offset = 0
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `
-}
-
-function loadPokemonItens(offset, limit) {
+function loadPokemonItems(offset, limit) {  
+    function convertPokemonToLi(pokemon) {
+        return `
+            <li class="pokemon ${pokemon.types[0]}"  id="${pokemon.number}">
+                <span class="number">${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+    
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+    
+                    <img src="${pokemon.photo}" 
+                         alt="${pokemon.name}">
+                </div>
+                <a href="details.html" class="btn ${pokemon.types[0]}" id="${pokemon.number}">Saber mais</a>
+            </li>
+        `
+    }
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        const newHtml = pokemons.map(convertPokemonToLi
+        ).join('')
         pokemonList.innerHTML += newHtml
-    })
+        //pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+        // Pega cada pokemon e cria uma lista de li, depois junta eles sem separador (''). Vira um html novo e concatena com o antigo.
+    }) 
 }
 
-loadPokemonItens(offset, limit)
+
+
+loadPokemonItems(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+    const qtdRecordNextPage = offset + limit
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
+    if( qtdRecordNextPage >= maxRecords ) {
         const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
-
+        loadPokemonItems(offset, newLimit)
         loadMoreButton.parentElement.removeChild(loadMoreButton)
     } else {
-        loadPokemonItens(offset, limit)
+        loadPokemonItems(offset, limit)
     }
 })
+
+function showDetailsPokemon(pokemon) {
+    return `
+        <div class="container-main">
+            <div class="container-name">
+                <span class="name">${pokemon.name}</span>
+                <span class="number">${pokemon.number}</span>
+            </div>
+            <div class="container-image">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        
+        </div>
+        <div class="container-details">
+            <div class="menu">
+                <span>Sobre</span>
+            </div>
+            <div class="about">
+                <div class="feature height">Height: ${pokemon.height}</d>
+                <div class="feature weight">Weight: ${pokemon.weight}</d>
+            </div>
+            <span class="types"></span>
+        </div>
+    `
+}
+
+const btn = document.getElementsByClassName('btn')
+btn.addEventListener('click', (e) => {
+    console.log(e);
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(showDetailsPokemon
+        ).join('')
+    })
+})
+  
+       
+
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -14,6 +14,10 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
+    pokemon.height = pokeDetail.height
+
+    pokemon.weight = pokeDetail.weight
+
     return pokemon
 }
 
